69d7693b0cca7ff1d0a6fe39ee505a0e068ecf20
pfreestone <pfreestone@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-14 16:19:22 +0000

    Formatting.

--- src/edu/drexel/info637/imudb/domain/Album.java
+++ src/edu/drexel/info637/imudb/domain/Album.java
@@ -1,21 +1,51 @@
 package edu.drexel.info637.imudb.domain;
+
 import java.util.Date;
 
-public class Album{
-    public int AlbumID=0;
-    public String sName="";
-    public Date rDate=null;
-    public String sRecordLabel="";
-    public String sProducer="";
-    
-    public void setAlbumID(int sID){AlbumID=sID;}
-    public int getAlbumID(){return AlbumID;}
-    public void setAlbumName(String aID){sName=aID;}
-    public String getArtistID(){return sName;}
-    public void setReleaseDate(Date sN){rDate=sN;}
-    public Date getSongName(){return rDate;}
-    public void setRecordLabel(String sAuth){sRecordLabel=sAuth;}
-    public String getRecordLabel(){return sRecordLabel;}
-    public void setProducer(String sB){sProducer=sB;}
-    public String getProducer(){return sProducer;}
+public class Album {
+    public int    AlbumID      = 0;
+    public String sName        = "";
+    public Date   rDate        = null;
+    public String sRecordLabel = "";
+    public String sProducer    = "";
+
+    public void setAlbumID(int sID) {
+        AlbumID = sID;
+    }
+
+    public int getAlbumID() {
+        return AlbumID;
+    }
+
+    public void setAlbumName(String aID) {
+        sName = aID;
+    }
+
+    public String getArtistID() {
+        return sName;
+    }
+
+    public void setReleaseDate(Date sN) {
+        rDate = sN;
+    }
+
+    public Date getSongName() {
+        return rDate;
+    }
+
+    public void setRecordLabel(String sAuth) {
+        sRecordLabel = sAuth;
+    }
+
+    public String getRecordLabel() {
+        return sRecordLabel;
+    }
+
+    public void setProducer(String sB) {
+        sProducer = sB;
+    }
+
+    public String getProducer() {
+        return sProducer;
+    }
 }
--- src/edu/drexel/info637/imudb/domain/User.java
+++ src/edu/drexel/info637/imudb/domain/User.java
@@ -1,21 +1,51 @@
 package edu.drexel.info637.imudb.domain;
+
 import java.util.Date;
 
-public class User{
-    public int UserID=0;
-    public String sUserName="";
-    public String sPass="";
-    public String sEmail="";
-    public String sAccessLevel="";
-    
-    public void setUserID(int iD){UserID=iD;}
-    public int getUserID(){return UserID;}
-    public void setUserName(String sN){sUserName=sN;}
-    public String getUserName(){return sUserName;}
-    public void setPass(String sP){sPass=sP;}
-    public String getPass(){return sPass;}
-    public void setEmail(String sE){sEmail=sE;}
-    public String getEmail(){return sEmail;}
-    public void setAccessLevel(String sA){sAccessLevel=sA;}
-    public String getAccessLevel(){return sAccessLevel;}
+public class User {
+    public int    UserID       = 0;
+    public String sUserName    = "";
+    public String sPass        = "";
+    public String sEmail       = "";
+    public String sAccessLevel = "";
+
+    public void setUserID(int iD) {
+        UserID = iD;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserName(String sN) {
+        sUserName = sN;
+    }
+
+    public String getUserName() {
+        return sUserName;
+    }
+
+    public void setPass(String sP) {
+        sPass = sP;
+    }
+
+    public String getPass() {
+        return sPass;
+    }
+
+    public void setEmail(String sE) {
+        sEmail = sE;
+    }
+
+    public String getEmail() {
+        return sEmail;
+    }
+
+    public void setAccessLevel(String sA) {
+        sAccessLevel = sA;
+    }
+
+    public String getAccessLevel() {
+        return sAccessLevel;
+    }
 }
