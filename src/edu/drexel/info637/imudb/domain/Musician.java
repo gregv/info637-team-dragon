93cb4770b115ae2a6d4eaa838e178fcf5ef25676
dimitrov84 <dimitrov84@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-14 05:37:17 +0000

    ddimitrov - initial batch of domain classes

---
+++ src/edu/drexel/info637/imudb/domain/Musician.java
@@ -0,0 +1,24 @@
+package edu.drexel.info637.imudb.domain;
+import java.util.Date;
+
+public class Musician{
+    public int MusicianID=0;
+    public String sName="";
+    public String sPlaceOfBirth="";
+    public Date DOB=null;
+    public Date DOD=null;
+    public String sBandAssociations="";
+    
+    public void setMusicianID(int iM){MusicianID=iM;}
+    public int getMusicianID(){return MusicianID;}
+    public void setName(String sN){sName=sN;}
+    public String getName(){return sName;}
+    public void setPlaceOfBirth(String sP){sPlaceOfBirth=sP;}
+    public String getPlaceOfBirth(){return sPlaceOfBirth;}
+    public void setDOB(Date dB){DOB=dB;}
+    public Date getDOB(){return DOB;}
+    public void setDOD(Date dD){DOD=dD;}
+    public Date getDOD(){return DOD;}
+    public void setBandAssociations(String sB){sBandAssociations=sB;}
+    public String getBandAssociations(){return sBandAssociations;}
+}
---
+++ src/edu/drexel/info637/imudb/domain/Track.java
@@ -0,0 +1,20 @@
+package edu.drexel.info637.imudb.domain;
+
+public class Track{
+    public int TrackID=0;
+    public int SongID=0;
+    public int AlbumID=0;
+    public int TrackNumber=0;
+    public int TrackLength=0;
+    
+    public void setSongID(int sID){SongID=sID;}
+    public int getSongID(){return SongID;}
+    public void setArtistID(int aID){TrackID=aID;}
+    public int getTrackID(){return TrackID;}
+    public void setAlbumID(int sN){AlbumID=sN;}
+    public int getAlbumID(){return AlbumID;}
+    public void setSongAuthor(int sAuth){TrackNumber=sAuth;}
+    public int getTrackNumber(){return TrackNumber;}
+    public void setTrackLength(int sB){TrackLength=sB;}
+    public int getTrackLength(){return TrackLength;}
+}
