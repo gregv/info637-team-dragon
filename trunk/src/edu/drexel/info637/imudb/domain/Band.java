c9983ad24f0b0ee7bf8ce2b093dda48c547c4c01
pfreestone <pfreestone@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-14 16:19:22 +0000

    Formatting.

--- trunk/src/edu/drexel/info637/imudb/domain/Band.java
+++ trunk/src/edu/drexel/info637/imudb/domain/Band.java
@@ -1,33 +1,87 @@
 package edu.drexel.info637.imudb.domain;
+
 import java.util.Date;
 
-public class Band{
-    public int BandID=0;
-    public String sName="";
-    public String sPlaceOfFormation="";
-    public Date YearOfFormation=null;
-    public String sExternalWebsite="";
-    public String sDescription="";
-    public String sInfluences="";
-    public String sHobbies="";
-    public int iGenreID=0;
-    
-    public void setBandID(int iB){BandID=iB;}
-    public int getBandID(){return BandID;}
-    public void setName(String sN){sName=sN;}
-    public String getName(){return sName;}
-    public void setPlaceFormed(String sP){sPlaceOfFormation=sP;}
-    public String getPlaceFormed(){return sPlaceOfFormation;}
-    public void setYearFormed(Date sY){YearOfFormation=sY;}
-    public Date getYearFormed(){return YearOfFormation;}
-    public void setExternalWebsite(String sE){sExternalWebsite=sE;}
-    public String getExternalWebsite(){return sExternalWebsite;}
-    public void setDescription(String sD){sDescription=sD;}
-    public String getDescription(){return sDescription;}
-    public void setInfluences(String sI){sInfluences=sI;}
-    public String getInfluences(){return sInfluences;}
-    public void setHobbies(String hB){sHobbies=hB;}
-    public String getHobbies(){return sHobbies;}
-    public void setGenreID(int iG){iGenreID=iG;}
-    public int getGenreID(){return iGenreID;}
+public class Band {
+    public int    BandID            = 0;
+    public String sName             = "";
+    public String sPlaceOfFormation = "";
+    public Date   YearOfFormation   = null;
+    public String sExternalWebsite  = "";
+    public String sDescription      = "";
+    public String sInfluences       = "";
+    public String sHobbies          = "";
+    public int    iGenreID          = 0;
+
+    public void setBandID(int iB) {
+        BandID = iB;
+    }
+
+    public int getBandID() {
+        return BandID;
+    }
+
+    public void setName(String sN) {
+        sName = sN;
+    }
+
+    public String getName() {
+        return sName;
+    }
+
+    public void setPlaceFormed(String sP) {
+        sPlaceOfFormation = sP;
+    }
+
+    public String getPlaceFormed() {
+        return sPlaceOfFormation;
+    }
+
+    public void setYearFormed(Date sY) {
+        YearOfFormation = sY;
+    }
+
+    public Date getYearFormed() {
+        return YearOfFormation;
+    }
+
+    public void setExternalWebsite(String sE) {
+        sExternalWebsite = sE;
+    }
+
+    public String getExternalWebsite() {
+        return sExternalWebsite;
+    }
+
+    public void setDescription(String sD) {
+        sDescription = sD;
+    }
+
+    public String getDescription() {
+        return sDescription;
+    }
+
+    public void setInfluences(String sI) {
+        sInfluences = sI;
+    }
+
+    public String getInfluences() {
+        return sInfluences;
+    }
+
+    public void setHobbies(String hB) {
+        sHobbies = hB;
+    }
+
+    public String getHobbies() {
+        return sHobbies;
+    }
+
+    public void setGenreID(int iG) {
+        iGenreID = iG;
+    }
+
+    public int getGenreID() {
+        return iGenreID;
+    }
 }
--- trunk/src/edu/drexel/info637/imudb/domain/Song.java
+++ trunk/src/edu/drexel/info637/imudb/domain/Song.java
@@ -1,20 +1,49 @@
 package edu.drexel.info637.imudb.domain;
 
-public class Song{
-    public int SongID=0;
-    public int ArtistID=0;
-    public String sSongName="";
-    public String sSongAuthor="";
-    public String sBand="";
-    
-    public void setSongID(int sID){SongID=sID;}
-    public int getSongID(){return SongID;}
-    public void setArtistID(int aID){ArtistID=aID;}
-    public int getArtistID(){return ArtistID;}
-    public void setSongName(String sN){sSongName=sN;}
-    public String getSongName(){return sSongName;}
-    public void setSongAuthor(String sAuth){sSongAuthor=sAuth;}
-    public String getSongAuthor(){return sSongAuthor;}
-    public void setBand(String sB){sBand=sB;}
-    public String getBand(){return sBand;}
+public class Song {
+    public int    SongID      = 0;
+    public int    ArtistID    = 0;
+    public String sSongName   = "";
+    public String sSongAuthor = "";
+    public String sBand       = "";
+
+    public void setSongID(int sID) {
+        SongID = sID;
+    }
+
+    public int getSongID() {
+        return SongID;
+    }
+
+    public void setArtistID(int aID) {
+        ArtistID = aID;
+    }
+
+    public int getArtistID() {
+        return ArtistID;
+    }
+
+    public void setSongName(String sN) {
+        sSongName = sN;
+    }
+
+    public String getSongName() {
+        return sSongName;
+    }
+
+    public void setSongAuthor(String sAuth) {
+        sSongAuthor = sAuth;
+    }
+
+    public String getSongAuthor() {
+        return sSongAuthor;
+    }
+
+    public void setBand(String sB) {
+        sBand = sB;
+    }
+
+    public String getBand() {
+        return sBand;
+    }
 }
