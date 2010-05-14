4ac51c37a5f54c2ceb36e19e18d6d5f8d3e4a11c
greg.vannoni <greg.vannoni@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-14 20:24:54 +0000

    Added search classes and AccessType/UserManager

---
+++ src/edu/drexel/info637/imudb/search/SearchResults.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.drexel.info637.imudb.search;
+
+/**
+ * @author Team Dragon
+ * @class INFO 637 Purpose:
+ * @version Notes:
+ */
+public class SearchResults {
+
+}
---
+++ src/edu/drexel/info637/imudb/user/AccessType.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.drexel.info637.imudb.user;
+
+/**
+ * @author Team Dragon
+ * @class INFO 637 Purpose:
+ * @version Notes:
+ */
+public enum AccessType {
+    MEMBER, CONTRIBUTOR, ADMINISTRATOR
+
+}
