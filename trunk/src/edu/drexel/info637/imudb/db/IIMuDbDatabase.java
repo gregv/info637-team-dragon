c9bbd24c26747313a7919548f3804e8a7c6913f7
pfreestone <pfreestone@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-15 04:08:28 +0000

    Merging db classes.

--- trunk/src/edu/drexel/info637/imudb/db/IIMuDbDatabase.java
+++ trunk/src/edu/drexel/info637/imudb/db/IIMuDbDatabase.java
@@ -1,25 +1,25 @@
-/**
- * 
- */
 package edu.drexel.info637.imudb.db;
 
 import java.util.ArrayList;
 
-import edu.drexel.info637.imudb.domain.User;
-import edu.drexel.info637.imudb.user.AccessType;
+import edu.drexel.info637.imudb.user.LoginResult;
 
 /**
- * @author Team Dragon
- * @class INFO 637 Purpose:
- * @version Notes:
+ * This interface is used to define methods that need to be implemented by the database class. This interface allows for a
+ * change in the underlying storage implementation without affecting any classes outside of the package.
+ * @author Patrick Freestone
+ *
  */
 public interface IIMuDbDatabase {
 
-    public AccessType loadUserAccessLevel(User user);
-
-    public String loadUserPassword(String username);
-
-    public User saveUser(User user);
+    /**
+     * This method will take a username and password and authenticate the user against the user list
+     * in the persistent data store
+     * @param userName  username to validate password for
+     * @param password  password for the given user
+     * @return          a LoginResult object with status set and error message populated if necessary
+     */
+    public LoginResult authenticateUser(String userName, String password);
 
     public ArrayList<ArrayList<Object>> requestSQLExecution(String SQLQuery);
 
