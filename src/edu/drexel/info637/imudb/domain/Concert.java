02f7b7940a1cf24158eca0c08cd6e7ecb318b31f
dimitrov84 <dimitrov84@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-15 05:35:36 +0000

    ddimitrov - changed public to private for the variables

--- src/edu/drexel/info637/imudb/domain/Concert.java
+++ src/edu/drexel/info637/imudb/domain/Concert.java
@@ -2,77 +2,30 @@ package edu.drexel.info637.imudb.domain;
 
 import java.util.Date;
 
-public class Concert {
-    public String sConcertName = "";
-    public String sBandName    = "";
-    public int    iBandID      = 0;
-    public int    iConcertID   = 0;
-    public Date   EventDate    = null;
-    public String sVenue       = "";
-    public String sTourName    = "";
-    public String sOrganizer   = "";
-
-    public void setConcertName(String sSet) {
-        sConcertName = sSet;
-    }
-
-    public String getConcertName() {
-        return sConcertName;
-    }
-
-    public void setBandName(String sB) {
-        sBandName = sB;
-    }
-
-    public String getBandName() {
-        return sBandName;
-    }
-
-    public void setBandID(int bID) {
-        iBandID = bID;
-    }
-
-    public int getBandID() {
-        return iBandID;
-    }
-
-    public void setConcertID(int iCID) {
-        iConcertID = iCID;
-    }
-
-    public int getConcertID() {
-        return iConcertID;
-    }
-
-    public void setEventDate(Date eDate) {
-        EventDate = eDate;
-    }
-
-    public Date getEventDate() {
-        return EventDate;
-    }
-
-    public void setVenue(String sV) {
-        sVenue = sV;
-    }
-
-    public String getVenue() {
-        return sVenue;
-    }
-
-    public void setTourName(String sT) {
-        sTourName = sT;
-    }
-
-    public String getTourName() {
-        return sTourName;
-    }
-
-    public void setOrganizer(String sO) {
-        sOrganizer = sO;
-    }
-
-    public String getOrganizer() {
-        return sOrganizer;
-    }
+public class Concert{
+    private String sConcertName="";
+    private String sBandName="";
+    private int iBandID=0;
+    private int iConcertID=0;
+    private Date EventDate=null;
+    private String sVenue="";
+    private String sTourName="";
+    private String sOrganizer="";
+    
+    public void setConcertName(String sSet){ sConcertName=sSet;}
+    public String getConcertName(){return sConcertName;}
+    public void setBandName(String sB){ sBandName=sB;}
+    public String getBandName(){return sBandName;}
+    public void setBandID(int bID){iBandID=bID;}
+    public int getBandID(){return iBandID;}
+    public void setConcertID(int iCID){iConcertID=iCID;}
+    public int getConcertID(){return iConcertID;}
+    public void setEventDate(Date eDate){EventDate=eDate;}
+    public Date getEventDate(){return EventDate;}
+    public void setVenue(String sV){ sVenue=sV;}
+    public String getVenue(){ return sVenue;}
+    public void setTourName(String sT){ sTourName=sT;}
+    public String getTourName(){return sTourName;}
+    public void setOrganizer(String sO){sOrganizer=sO;}
+    public String getOrganizer(){return sOrganizer;}
 }
--- src/edu/drexel/info637/imudb/domain/Song.java
+++ src/edu/drexel/info637/imudb/domain/Song.java
@@ -1,49 +1,20 @@
 package edu.drexel.info637.imudb.domain;
 
-public class Song {
-    public int    SongID      = 0;
-    public int    ArtistID    = 0;
-    public String sSongName   = "";
-    public String sSongAuthor = "";
-    public String sBand       = "";
-
-    public void setSongID(int sID) {
-        SongID = sID;
-    }
-
-    public int getSongID() {
-        return SongID;
-    }
-
-    public void setArtistID(int aID) {
-        ArtistID = aID;
-    }
-
-    public int getArtistID() {
-        return ArtistID;
-    }
-
-    public void setSongName(String sN) {
-        sSongName = sN;
-    }
-
-    public String getSongName() {
-        return sSongName;
-    }
-
-    public void setSongAuthor(String sAuth) {
-        sSongAuthor = sAuth;
-    }
-
-    public String getSongAuthor() {
-        return sSongAuthor;
-    }
-
-    public void setBand(String sB) {
-        sBand = sB;
-    }
-
-    public String getBand() {
-        return sBand;
-    }
+public class Song{
+    private int SongID=0;
+    private int ArtistID=0;
+    private String sSongName="";
+    private String sSongAuthor="";
+    private String sBand="";
+    
+    public void setSongID(int sID){SongID=sID;}
+    public int getSongID(){return SongID;}
+    public void setArtistID(int aID){ArtistID=aID;}
+    public int getArtistID(){return ArtistID;}
+    public void setSongName(String sN){sSongName=sN;}
+    public String getSongName(){return sSongName;}
+    public void setSongAuthor(String sAuth){sSongAuthor=sAuth;}
+    public String getSongAuthor(){return sSongAuthor;}
+    public void setBand(String sB){sBand=sB;}
+    public String getBand(){return sBand;}
 }
