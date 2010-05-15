f73d221b60622bcc9ef4c4f80eaaff3de6d4bbdb
pfreestone <pfreestone@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-15 04:55:00 +0000

    Adding login funcitonality.

--- trunk/src/edu/drexel/info637/imudb/servlet/LoginPageServlet.java
+++ trunk/src/edu/drexel/info637/imudb/servlet/LoginPageServlet.java
@@ -1,8 +1,18 @@
 package edu.drexel.info637.imudb.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.drexel.info637.imudb.db.DBObject;
+import edu.drexel.info637.imudb.domain.User;
+import edu.drexel.info637.imudb.user.LoginResult;
 
 /**
  * This class is responsible for processing a user login request and either redirecting them back to the main page
@@ -10,6 +20,11 @@ import javax.servlet.http.HttpServletResponse;
  * @author Patrick Freestone
  */
 public class LoginPageServlet extends HttpServlet {
+
+    private static String FAILED_LOGIN_HTML_HEAD = "<html><p>";
+    private static String FAILED_LOGIN_HTML_TAIL = "</p><p><a href=\"login.jsp\">Retry Login</a></p><p><a href=\"index.html\">Home Page</a></p></html>";
+
+    PrintWriter           out                    = null;
 
     /**
      * Overrides the parent's method and forwards the request to the processRequest method. 
@@ -19,7 +34,7 @@ public class LoginPageServlet extends HttpServlet {
      * }
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+        processRequest(request, response);
     }
 
     /**
@@ -30,7 +45,7 @@ public class LoginPageServlet extends HttpServlet {
      * }
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+        processRequest(request, response);
     }
 
     /**
@@ -40,8 +55,8 @@ public class LoginPageServlet extends HttpServlet {
      * {
      *      1. set userName from request parameter
      *      2. set password from request parameter
-     *      3. load User object from database using username
-     *      4. if (password from request == password from database) {
+     *      3. if (LoginResult is success) {
+     *          4. load User object from LoginResult
      *          5. set session to have user object of the authenticated user
      *          6. forward the request back to the main page
      *      } 7. else {
@@ -52,6 +67,34 @@ public class LoginPageServlet extends HttpServlet {
      * @param response  returned page; either forwarded to the main page or reload the login page with an error
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+        LoginResult loginResult = DBObject.getInstance().authenticateUser(userName, password);
+
+        if (loginResult.isSuccess()) {
+            HttpSession session = request.getSession(true);
+            User loggedInUser = loginResult.getUser();
+            session.setAttribute("user", loggedInUser);
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/servlet/MainPageServlet");
+            try {
+                dispatcher.include(request, response);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } catch (ServletException se) {
+                se.printStackTrace();
+            }
+        } else {
+            try {
+                out = response.getWriter();
+                out.print(FAILED_LOGIN_HTML_HEAD);
+                out.print(loginResult.getErrorMsg());
+                out.print(FAILED_LOGIN_HTML_TAIL);
+
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
 
     }
 
