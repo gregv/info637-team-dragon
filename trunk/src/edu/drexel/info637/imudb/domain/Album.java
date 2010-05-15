7dac9096db78053d834732b887f1dfe39a36bc08
dimitrov84 <dimitrov84@8f04a9d9-ee3c-8caf-4ea7-9a53d74f3f23>
2010-05-15 05:35:36 +0000

    ddimitrov - changed public to private for the variables

--- trunk/src/edu/drexel/info637/imudb/domain/Album.java
+++ trunk/src/edu/drexel/info637/imudb/domain/Album.java
@@ -1,51 +1,21 @@
 package edu.drexel.info637.imudb.domain;
-
 import java.util.Date;
 
-public class Album {
-    public int    AlbumID      = 0;
-    public String sName        = "";
-    public Date   rDate        = null;
-    public String sRecordLabel = "";
-    public String sProducer    = "";
-
-    public void setAlbumID(int sID) {
-        AlbumID = sID;
-    }
-
-    public int getAlbumID() {
-        return AlbumID;
-    }
-
-    public void setAlbumName(String aID) {
-        sName = aID;
-    }
-
-    public String getArtistID() {
-        return sName;
-    }
-
-    public void setReleaseDate(Date sN) {
-        rDate = sN;
-    }
-
-    public Date getSongName() {
-        return rDate;
-    }
-
-    public void setRecordLabel(String sAuth) {
-        sRecordLabel = sAuth;
-    }
-
-    public String getRecordLabel() {
-        return sRecordLabel;
-    }
-
-    public void setProducer(String sB) {
-        sProducer = sB;
-    }
-
-    public String getProducer() {
-        return sProducer;
-    }
+public class Album{
+    private int AlbumID=0;
+    private String sName="";
+    private Date rDate=null;
+    private String sRecordLabel="";
+    private String sProducer="";
+    
+    public void setAlbumID(int sID){AlbumID=sID;}
+    public int getAlbumID(){return AlbumID;}
+    public void setAlbumName(String aID){sName=aID;}
+    public String getArtistID(){return sName;}
+    public void setReleaseDate(Date sN){rDate=sN;}
+    public Date getSongName(){return rDate;}
+    public void setRecordLabel(String sAuth){sRecordLabel=sAuth;}
+    public String getRecordLabel(){return sRecordLabel;}
+    public void setProducer(String sB){sProducer=sB;}
+    public String getProducer(){return sProducer;}
 }
--- trunk/src/edu/drexel/info637/imudb/domain/User.java
+++ trunk/src/edu/drexel/info637/imudb/domain/User.java
@@ -1,5 +1,23 @@
 package edu.drexel.info637.imudb.domain;
-
+<<<<<<< .mine
+public class User{
+    private int UserID=0;
+    private String sUserName="";
+    private String sPass="";
+    private String sEmail="";
+    private String sAccessLevel="";
+    
+    public void setUserID(int iD){UserID=iD;}
+    public int getUserID(){return UserID;}
+    public void setUserName(String sN){sUserName=sN;}
+    public String getUserName(){return sUserName;}
+    public void setPass(String sP){sPass=sP;}
+    public String getPass(){return sPass;}
+    public void setEmail(String sE){sEmail=sE;}
+    public String getEmail(){return sEmail;}
+    public void setAccessLevel(String sA){sAccessLevel=sA;}
+    public String getAccessLevel(){return sAccessLevel;}
+}=======
 public class User {
     private int    id           = 0;
     private String userName     = "";
@@ -48,3 +66,4 @@ public class User {
     }
 
 }
+>>>>>>> .r101
